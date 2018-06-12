339d1496dd8f72eb5d132bb9bb51a28c2324df93
杜鹏 <dupeng.tommy@outlook.com>
2018-06-12 18:37:15 +0800

    查询我的订单

--- ShiJieBei/Controllers/GameOrderController.cs
+++ ShiJieBei/Controllers/GameOrderController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ShiJieBei.Models;
 using ShiJieBeiComponents.Domains;
 using ShiJieBeiComponents.Helpers;
 
@@ -48,6 +49,27 @@ namespace ShiJieBei.Controllers
             CurrentUser.Account.Vouchers -= 10;
             _db.SaveChanges();
             return Json(new { success = true });
+        }
+
+        public ActionResult MyGameList()
+        {
+
+            var gameList = CurrentUser.GameOrders.ToList().Join(_db.Games, go => go.GameId, g => g.Id, (go, g) => new { CreateTime = go.CreateTime.ToString("yyyy-MM-dd HH:mm:ss"), Number = go.Number, go.GameOrderStatus, g.KeChang, g.ZhuChang, StartTime = g.StartTime.ToString("yyyy-MM-dd HH:mm:ss") });
+
+            List<MyGameListViewModel> myGameListViewModel = new List<MyGameListViewModel>();
+            foreach (var item in gameList)
+            {
+                myGameListViewModel.Add(new MyGameListViewModel
+                {
+                    Number = item.Number,
+                    CreateTime = item.CreateTime,
+                    GameOrderStatus = item.GameOrderStatus,
+                    KeChang = item.KeChang,
+                    ZhuChang = item.ZhuChang,
+                    StartTime = item.StartTime
+                });
+            }
+            return View(myGameListViewModel);
         }
     }
 }
---
+++ ShiJieBei/Models/MyGameListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ShiJieBeiComponents.Domains;
+
+namespace ShiJieBei.Models
+{
+    public class MyGameListViewModel
+    {
+        public string Number { get; set; }
+        public string CreateTime { get; set; }
+        public GameOrderStatus GameOrderStatus { get; set; }
+        public string  KeChang { get; set; }
+        public string ZhuChang { get; set; }
+        public string StartTime { get; set; }
+    }
+}
