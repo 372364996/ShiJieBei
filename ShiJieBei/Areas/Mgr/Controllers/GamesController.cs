cbb2f9f20b42844f28b9598cf20192ed998d9789
杜鹏 <dupeng.tommy@outlook.com>
2018-06-18 14:55:39 +0800

    赛程管理

---
+++ ShiJieBei/Areas/Mgr/Controllers/GamesController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ShiJieBeiComponents.Domains;
+using ShiJieBeiComponents.Repositories.EF;
+
+namespace ShiJieBei.Areas.Mgr.Controllers
+{
+    public class GamesController : MgrController
+    {
+        private ShiJieBeiDbContext db = new ShiJieBeiDbContext();
+
+        // GET: Mgr/Games
+        public ActionResult Index()
+        {
+            return View(db.Games.ToList());
+        }
+
+        // GET: Mgr/Games/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Games games = db.Games.Find(id);
+            if (games == null)
+            {
+                return HttpNotFound();
+            }
+            return View(games);
+        }
+
+        // GET: Mgr/Games/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Mgr/Games/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,ZhuChang,KeChang,ZhuChangSuoXie,KeChangSuoXie,ZhuChangScore,KeChangScore,Status,StartTime")] Games games)
+        {
+            if (ModelState.IsValid)
+            {
+                games.CreateTime = DateTime.Now;
+                db.Games.Add(games);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(games);
+        }
+
+        // GET: Mgr/Games/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Games games = db.Games.Find(id);
+            if (games == null)
+            {
+                return HttpNotFound();
+            }
+            return View(games);
+        }
+
+        // POST: Mgr/Games/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,ZhuChang,KeChang,ZhuChangSuoXie,KeChangSuoXie,ZhuChangScore,KeChangScore,Status,StartTime")] Games games)
+        {
+            if (ModelState.IsValid)
+            {
+                games.CreateTime = DateTime.Now;
+                db.Entry(games).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(games);
+        }
+
+        // GET: Mgr/Games/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Games games = db.Games.Find(id);
+            if (games == null)
+            {
+                return HttpNotFound();
+            }
+            return View(games);
+        }
+
+        // POST: Mgr/Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Games games = db.Games.Find(id);
+            db.Games.Remove(games);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- ShiJieBeiComponents/Domains/Games.cs
+++ ShiJieBeiComponents/Domains/Games.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,24 @@ namespace ShiJieBeiComponents.Domains
     public class Games
     {
         public int Id { get; set; }
+       
+        [ Display(Name = "主场")]
         public string ZhuChang { get; set; }
+        [Display(Name = "客场")]
         public string KeChang { get; set; }
+        [Display(Name = "主场缩写")]
         public string ZhuChangSuoXie { get; set; }
+        [Display(Name = "客场缩写")]
         public string KeChangSuoXie { get; set; }
+        [Display(Name = "主场得分")]
         public int ZhuChangScore { get; set; }
+        [Display(Name = "客场得分")]
         public int KeChangScore { get; set; }
+        [Display(Name = "比赛结果")]
         public GameOrderStatus Status { get; set; }
+        [Display(Name = "创建时间")]
         public DateTime CreateTime { get; set; }
+        [Display(Name = "比赛时间")]
         public DateTime StartTime { get; set; }
         public virtual List<GameOrders> GameOrders { get; set; }
     }
